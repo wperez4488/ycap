dd1fc34165b7fe6ee761709ab46380fab7683986
Hugo Zuniga <hzuniga@Macbook-hugz.local>
2018-08-31 19:16:35 -0400

    added codekit support, auto refresh,backed up og stylesheet renamed it style1.scss, added sass folder and scss bootstrap4

--- wp-content/themes/Ycap2019/header.php
+++ wp-content/themes/Ycap2019/header.php
@@ -16,7 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory');?>/assets/css/bootstrap.min.css">
+	<!-- <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory');?>/assets/css/bootstrap.min.css"> -->
 	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 
