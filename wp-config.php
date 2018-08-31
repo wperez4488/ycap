a9f2fa4d372a3ca58fba40c7a709761a8eec4039
Hugo Zuniga <hzuniga@Macbook-hugz.local>
2018-08-31 15:37:47 -0400

    local db added to wp-config, and codekit support added to theme

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'ycap');
 
 /** MySQL database username */
-define('DB_USER', 'admin');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'jBPueqnN5ivgSWFb');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
