ba4e7e028ea198f04f6071602f22b37b7ad79375
Hugo Zuniga <hzuniga@Macbook-hugz.local>
2018-08-31 21:37:03 -0400

    front-page added

--- wp-content/themes/Ycap2019/front-page.php
+++ wp-content/themes/Ycap2019/front-page.php
@@ -1,18 +1,5 @@
 <?php
-/**
- Template Name: Home page
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package ycap
 
-
- */
 $blurb_1 = get_field('blurb1');
 $statement = get_field('statement');
 $partner_1 = get_field('partner_1');
