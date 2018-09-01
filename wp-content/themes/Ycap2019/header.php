88803c24046e551198db2a51dbc5bd4136a2e823
Hugo Zuniga <hzuniga@Macbook-hugz.local>
2018-09-01 18:37:11 -0400

    header structure completed

--- wp-content/themes/Ycap2019/header.php
+++ wp-content/themes/Ycap2019/header.php
@@ -16,33 +16,67 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<!-- <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory');?>/assets/css/bootstrap.min.css"> -->
-	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
-<!-- 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
- -->
+
 
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-		<div class="navbar custom">
-			<div class="container">
-			<div class="navbar-brand">
-				<img src="<?php bloginfo('stylesheet_directory');?>/assets/img/logo2x.png" alt="">
-			</div>
-		</div>
-		</div>
+		
+	<header>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'ycap' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'header',
-			) );
-			?>
-			<li><i class="fas fa-chevron-right"></i>abc</li>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+		<nav class="top-navigation">
+		  <div class="main-container">
+		  	<!-- wp custome logo -->
+		  	<?php the_custom_logo( $blog_id = 0 ); ?>
+		  	<!-- <a class="navbar-brand" href="#">
+		  		<img src="/assets/img/logo2x.png" alt="">
+		  	  </a> -->
+		  	
+		  	<div class="form-inline">
+		  	  <a class="btn btn-primary" data-toggle="modal"data-target="#exampleModal" href="">
+		  	  	<i class="fas fa-bars"></i>
+		  	  </a>
+		  				
+				<!-- Modal -->
+				<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+				  <div class="modal-dialog" role="document">
+				    <div class="modal-content">
+				      <div class="modal-header">
+				        <h5 class="modal-title" id="exampleModalLabel">Menu</h5>
+				        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+				          <span aria-hidden="true">&times;</span>
+				        </button>
+				      </div>
+				      <div class="modal-body">
+				      	<!-- registerd 'menu-1' in functions.php -->
+				        <?php 
+		                    wp_nav_menu( array(
+		                            'theme_location'    => 'menu-1',
+		                            'container'         => '',
+		                            'container_class'   => '',
+		                            'menu_class'        => 'menu-1 nav navbar-nav',
+		                            'fallback_cb'       => 'wp_bootstrap_navwalker::fallback'
+		                            // 'walker'            => new wp_bootstrap_navwalker()
+		                        )
+		                    );
+		                ?>
+				      </div>
+				      <div class="modal-footer">
+				       <!--  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				        <button type="button" class="btn btn-primary">Save changes</button> -->
+				      </div>
+				    </div>
+				  </div>
+				</div>
+				<!-- end modal -->
+		  	</div>
+		  </div>
+		</nav>
+
+
+	</header>
+
+
 
 	<div id="content" class="site-content">
---
+++ wp-content/themes/Ycap2019/template-parts/jumbotron.php
@@ -0,0 +1,27 @@
+<div class="hero">
+	<div class="container">
+		<div class="wrap">
+			<div class="header text-left">
+				<h1>Yonkers<br> Community <br> <span class="yellow">Action Program</span></h1>
+			<hr>
+			</div>
+			
+			<div class="subline">
+				
+				<p>Helping People. Changing Lives</p>
+			</div> 
+
+			<div class="hugs">
+				<img src="/assets/img/hugs.png" alt="">
+				<div class="content col-md-6">
+					<?php echo $blurb_1;?>
+				</div>
+			</div>
+			
+			<div class="group">
+				<img src="/assets/img/Group.png" alt="">
+			</div>
+
+		</div>
+	</div>
+</div>
